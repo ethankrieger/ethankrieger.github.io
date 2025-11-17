94974857c37248a53bd37398029172b62b53cf07
sayheyakanksha <sayheyakanksha@gmail.com>
2025-11-17 02:02:23 -0500

    fixed video games quotes formatting

--- assets/js/video-game-quotes.js
+++ assets/js/video-game-quotes.js
@@ -1,8 +1,9 @@
 fetch('video-game-quotes.json')
-    .then(response => response.json())
-    .then(quotes => {
-      const randomIndex = Math.floor(Math.random() * quotes.length);
-      const quoteElement = document.getElementById('quote');
-      quoteElement.textContent = quotes[randomIndex].quote;
-    })
-    .catch(error => console.error('Error loading quotes:', error));
+  .then(response => response.json())
+  .then(quotes => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const { quote, author } = quotes[randomIndex];
+    const quoteElement = document.getElementById('quote');
+    quoteElement.textContent = `“${quote}” — ${author}`;
+  })
+  .catch(error => console.error('Error loading quotes:', error));
